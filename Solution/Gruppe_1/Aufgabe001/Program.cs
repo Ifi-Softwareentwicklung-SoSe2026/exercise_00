d5413f731af8afe562a4e67889f90b019f693647
Volker Goehler <volker.goehler@informatik.tu-freiberg.de>
2026-04-27 08:33:58 +0200

    added solution for first part

---
+++ Solution/Gruppe_1/Aufgabe001/Program.cs
@@ -0,0 +1,83 @@
+﻿public enum Typ
+{
+    Stern,
+    Planet,
+    Mond
+}
+
+
+public class Himmelskoerper
+{
+    public string Name { get; set;}
+    public uint KatalogNummer { get; set;}
+    // typ
+    public Typ Himmelskoerpertyp { get; set;}
+    // spectral
+    public char? Spectral { get; set;}
+    // helligkeit
+    public float? helligkeit { get; set;}
+    // Umlauf
+    public float? umlauf { get; set;}
+    // KatNr von Elternobjekt
+    public uint KatalogNummerReferenz { get; set;}    
+
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Himmelskoerper koerper = new Himmelskoerper();
+        Console.WriteLine(args[1]);
+        LeseEin(koerper, args);
+        Ausgabe(koerper);
+    }
+
+    public static void Ausgabe(Himmelskoerper koerper)
+    {
+
+    }
+
+    public static void LeseEin(Himmelskoerper koerper, string[] args)
+    {
+        if (args.Length > 1)
+        {
+            koerper.Name = args[1];
+            koerper.KatalogNummer = uint.Parse(args[2]);
+            koerper.Himmelskoerpertyp = (Typ) Enum.Parse(typeof(Typ), args[3]);
+            if (koerper.Himmelskoerpertyp == Typ.Stern)
+            {
+                koerper.Spectral = args[4][0];
+                koerper.helligkeit = float.Parse(args[5]);
+            }else{
+                koerper.umlauf = float.Parse(args[4]);
+                koerper.KatalogNummerReferenz = uint.Parse(args[5]);
+            }
+        }else{
+            Console.WriteLine("Name des Himmelskörpers:");
+            koerper.Name = Console.ReadLine();
+
+            Console.WriteLine("KatalogNummer (5-stellig):");
+            koerper.KatalogNummer = uint.Parse(Console.ReadLine());
+
+            Console.WriteLine("Geben Sie den Typ ein (Stern, Planet, Mond):");
+            koerper.Himmelskoerpertyp = (Typ) Enum.Parse(typeof(Typ), Console.ReadLine());
+
+            if (koerper.Typ == Typ.Stern)
+            {
+                Console.WriteLine("Spektralklasse (O, B, A, F, G, K, M): ");
+                koerper.Spektralklasse = Console.ReadLine()[0];
+
+                Console.WriteLine("scheinbare Helligkeit: ");
+                koerper.helligkeit = float.Parse(Console.ReadLine());
+            }else{
+                Console.WriteLine("Umlaufzeit in Erdjahren: ");
+                koerper.umlauf = float.Parse(Console.ReadLine());
+                Console.WriteLine("KatalogNummer des Zentralkörpers: ");
+                koerper.KatalogNummerReferenz = uint.Parse(Console.ReadLine());
+            }
+        }
+
+
+    }
+}
---
+++ Solution/Gruppe_2/Aufgabe001/Program.cs
@@ -0,0 +1,124 @@
+﻿public enum HimmelskoerperTyp
+{
+    Stern,
+    Planet,
+    Mond,
+    Todestern
+}
+
+public class Himmelskoerper
+{
+    public string Name { get; set;}
+    // katalognummer
+    public uint KatalogNummer { get; set;}
+    // HimmelskoerperTyp
+    public HimmelskoerperTyp Typ { get; set;}
+    // Spektralklasse
+    public char? SpektralKlasse { get; set;}
+    // ScheinbareHelligkeit
+    public double? ScheinbareHelligkeit { get; set;}
+    // Umlaufzeit
+    public double? Umlaufzeit { get; set;}
+    // ZentralkoerperKatalogNummer
+    public uint? ZentralkoerperKatalogNummer { get; set;}
+}
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Himmelskoerper stern = new Himmelskoerper();
+        LeseDatenEin(stern, args);
+        var istOkay = Validierung(stern);
+        if (istOkay)
+        {
+            GibDatenAus(stern);
+        }
+        else
+        {
+            Console.WriteLine("Fehlerhafte Eingabe.");
+        }
+    }
+
+    public static bool Validierung(Himmelskoerper koerper)
+    {
+        // Zentralkoerper KatalogNummer muss 5 Stellig sein
+        if (koerper.ZentralkoerperKatalogNummer < 10000 ||
+         koerper.ZentralkoerperKatalogNummer > 99999)
+        {
+            return false;
+        }
+        if (koerper.KatalogNummer < 10000 ||
+         koerper.KatalogNummer > 99999)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public static void GibDatenAus(Himmelskoerper koerper)
+    {
+        Console.WriteLine("Himmelskörper:");
+        Console.WriteLine($"Name: {koerper.Name}, Katalog-Nummer: {koerper.KatalogNummer}, Typ: {koerper.Typ}");
+        if (koerper.Typ == Himmelskoerper.Stern)
+        {
+            Console.WriteLine($"Spektralklasse: {koerper.Spektralklasse}, Scheinbare Helligkeit: {koerper.ScheinbareHelligkeit}");
+        }else{
+            Console.WriteLine($"Umlaufzeit: {koerper.Umlaufzeit} Erdjahre, Zentralkörper-Katalognummer: {koerper.ZentralkoerperKatalogNummer}");
+        }
+    }
+
+    public static void LeseDatenEin(Himmelskoerper koerper, string[] args)
+    {
+        if (args.Length > 2){
+            koerper.Name = args[1];
+            koerper.KatalogNummer = uint.Parse(args[2]);
+            koerper.Typ = (HimmelskoerperTyp)Enum.Parse(typeof(HimmelskoerperTyp), args[3]);
+
+            if (koerper.Typ == HimmelskoerperTyp.Stern)
+            {
+                // SpektralKlasse
+                koerper.SpektralKlasse = args[4][0]; //char.Parse(args[4])
+                // ScheinbareHelligkeit
+                koerper.ScheinbareHelligkeit = double.Parse(args[5]);
+            }else{
+                // Umlaufzeit
+                koerper.Umlaufzeit = double.Parse(args[4]);
+                // ZentralkoerperKatalogNummer
+                koerper.ZentralkoerperKatalogNummer = uint.Parse(args[5]);
+            }
+        }else{
+            Console.WriteLine("Namen des Himmelskörpers: ");
+            koerper.Name = Console.ReadLine();
+
+            Console.WriteLine("Katalognummer (5-Stellig): ");
+            koerper.KatalogNummer = uint.Parse(Console.ReadLine());
+
+            Console.WriteLine("Himmelskörpertyp (Stern, Planet, Mond): ");
+            koerper.Typ = (HimmelskoerperTyp)Enum.Parse(
+                typeof(HimmelskoerperTyp),
+                Console.ReadLine()
+                );
+
+            if (koerper.Typ == HimmelskoerperTyp.Stern)
+            {
+                // SpektralKlasse
+                Console.WriteLine("Spektralklasse: ");
+                koerper.SpektralKlasse = char.Parse(Console.ReadLine());
+                // ScheinbareHelligkeit
+                Console.WriteLine("Scheinbare Helligkeit: ");
+                koerper.ScheinbareHelligkeit = double.Parse(Console.ReadLine());
+            }else{
+                // Umlaufzeit
+                Console.WriteLine("Umlaufzeit: ");
+                koerper.Umlaufzeit = double.Parse(Console.ReadLine());
+                // ZentralkoerperKatalogNummer
+                Console.WriteLine("Katalognummer des Zentralgestirns: ");
+                koerper.ZentralkoerperKatalogNummer = uint.Parse(Console.ReadLine());
+            }
+        }
+    }
+    
+    //public static Himmelskoerper LeseDatenEin(string[] args){}
+}
